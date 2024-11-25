248b16894e56c4fc041da498cef001a8469a4388
Mark Hahn <mark@hahnca.com>
2024-11-24 19:30:35 -0800

    broken list

--- src/emby.js
+++ src/emby.js
@@ -33,7 +33,6 @@ export async function init() {
 
 ////////////////////////  UTILITIES  ///////////////////////
 
-// TODO broken
 export async function providers (show) {
   const url = `http://hahnca.com:8096/emby/Items?Recursive=true&Fields=ProviderIds&Ids=${show.Id}&api_key=ba7d62f79cbd4a539b675b05b5663607`;
   const item = (await axios.get(url)).data.Items[0];
--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+/*
+Broken ...
+  broken emby click on app load
+  broken sort on app load
+  font sizes and families
+  gap
+  ban
+  show in emby (computer icon)
+  ctrl-click(imdb) (providers)
+*/
+
 import {createApp} from 'vue'
 import App from './App.vue'
 

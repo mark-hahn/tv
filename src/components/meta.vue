5f3545ffb7caf69a7dd9b74979796bffc434f6ba
mark-hahn <mark@hahnca.com>
2024-12-30 16:26:05 -0800

    split app into list and meta

---
+++ src/components/meta.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+
+#meta(style=`width:40%; height:95dvh;
+                 padding:0; margin:0; display:flex;`)
+  div(style=`border:0.5px solid gray;`) {{show}}
+
+</template>
+
+<script>
+const imgNames = ['clearlogo', 'landscape', 'poster'];
+const imgUrl = (show, imgName) =>
+        `https://hahnca.com/tv/${encodeURI(show.Name)}/${imgName}`
+//- console.log(imgUrl({Name: 'Dollface'}, 'clearlogo.png'));
+
+export default {
+  name: "Meta",
+  components: {},
+  inject: ['evtBus'],
+
+  data() {
+    return {
+      show: '<No Show Selected>',
+    }
+  },
+  
+  methods: {
+  
+    mounted() {
+      this.evtBus.on('showSelected', (show) => { 
+        console.log('Meta: showSelected:', show);
+        this.show = show;
+      });
+    }
+
+  },
+};
+</script>

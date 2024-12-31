cc5e2a0d3254e8b94527dc31797df3c82f2ffe9c
mark-hahn <mark@hahnca.com>
2024-12-31 14:56:07 -0800

    added emby image options

--- src/components/meta.vue
+++ src/components/meta.vue
@@ -3,13 +3,11 @@
 #meta(style=`height:95dvh;
                  padding:0; margin:0; display:flex;`)
   div(style=`border:0.5px solid gray;`) name: {{show.Name}}
-  #poster()
+  #poster(style=`width:250px; height:500px;`)
 </template>
 
 <script>
 import evtBus from '../evtBus.js';
-
-const images = ['/poster.jpg', '/landscape.jpg', '/clearlogo.png'];
 
 export default {
   name: "Meta",
@@ -21,25 +19,49 @@ export default {
   
   methods: {
     setPoster() {
+      const show = this.show;
+      const srvrPath = show.Path.split('/').pop();
+      const srvrImages = 
+              ['/poster.jpg', '/landscape.jpg', '/clearlogo.png'];
+      const embyImages = [
+        `https://hahnca.com:8920/emby/Items/${show.Id}` +
+          `/Images/Primary?maxHeight=802&maxWidth=534&` +
+          `tag=${show.ImageTags.Primary}`               +
+          `&keepAnimation=true&quality=90`,
+        `https://hahnca.com:8920/emby/Items/${show.Id}` +
+        `/Images/Backdrop/0?tag=${show.BackdropImageTags[0]}` +
+        `&maxWidth=2560&quality=70`
+      ];
       const img = new Image();
       let imgIdx = 0;
-      const tryImg = () => {
-        console.log('Meta: trying img:',  images[imgIdx]);
+      const trySrvrImg = () => {
         img.src = 'https://hahnca.com/tv/' +
-              encodeURI(this.show.Name) + images[imgIdx]; 
+                     encodeURI(srvrPath) + srvrImages[imgIdx]; 
+        console.log('Meta: trying srvr img:',  img.src);
       };
-      tryImg();
+      const tryEmbyImg = () => {
+        img.src = embyImages[imgIdx-srvrImages.length]; 
+        console.log('Meta: trying emby img:',  img.src);
+      }
+      trySrvrImg();
       img.onload = () => {
-        console.log('Meta: showing img:',  images[imgIdx]);
+        console.log('Meta showing img:',  img.src);
         document.getElementById('poster').replaceChildren(img);
       };
       img.onerror = () => {
-        console.log('Meta: no img:',  images[imgIdx]);
-        if(++imgIdx == images.length) {
-          console.log('Meta: no image found');
+        // console.log('Meta no img:', img.src);
+        if(++imgIdx < srvrImages.length) {
+          trySrvrImg();
+        }
+        else if(++imgIdx < srvrImages.length + 
+                           embyImages.length) {
+          tryEmbyImg();
+        }
+        else {
+          img.src = 'https://hahnca.com/tv/no-image.png'; 
+          console.log( `Meta default img: ` + img.src);
           return;
         }
-        tryImg();
       };
     },
   },

6681a4893cf7dfd582621028581d510788332e33
Mark Hahn <mark@hahnca.com>
2025-01-05 19:55:39 -0800

    clnup

--- src/lastViewed.js
+++ src/lastViewed.js
@@ -1,29 +1,29 @@
-import fs        from "fs";
-import * as fsp  from 'fs/promises';
+import fs from "fs";
+import * as fsp from 'fs/promises';
 import * as emby from "./emby.js";
 
-const lastViewedStr = 
-        fs.readFileSync('data/lastViewed.json', 'utf8');
+const lastViewedStr =
+  fs.readFileSync('data/lastViewed.json', 'utf8');
 const lastViewed = JSON.parse(lastViewedStr);
 
 const checkWatch = async () => {
   const showName = await emby.getCurrentlyWatching();
-  if(showName !== null) 
-      lastViewed[showName] = Date.now();
-  if(showName != lastShowName) {
-      await fsp.writeFile('data/lastViewed.json', 
-                            JSON.stringify(lastViewed));
+  if (showName !== null)
+    lastViewed[showName] = Date.now();
+  if (showName != lastShowName) {
+    await fsp.writeFile('data/lastViewed.json',
+      JSON.stringify(lastViewed));
   }
   lastShowName = showName;
-  
+
   // console.log({lastShowName, showName, 
   //                   lastViewed: JSON.stringify(lastViewed, null,2)});
 }
 
 let lastShowName = null;
-setInterval(async ()=> {
+setInterval(async () => {
   await checkWatch();
-}, 5*60*1000);  // 5 mins
+}, 5 * 60 * 1000);  // 5 mins
 
 export const getLastViewed = (id, _param, resolve, _reject) => {
   resolve([id, lastViewed]);

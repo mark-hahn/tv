a98fee06c45f07ecfe0e8fc871ceebdcee51e6fe
mark-hahn <mark@hahnca.com>
2025-01-11 17:25:49 -0800

    bkup

--- src/gap-worker.js
+++ src/gap-worker.js
@@ -3,27 +3,67 @@ import * as urls from "./urls.js";
 
 let cred;
 
-const getShowState = async (showId, showName) => {
-  // active rows have watched or last epi in last row watched
-  let ready      = false;  // first non-watched has file
-  let waiting    = false;  // unaired in active row
-  let watchedGap = false; 
-  let fileGap    = false; 
-  let allWatched = true;
-  let haveWatched = false;
-  let haveFile    = false;
+const getShowState = async (showId, _showName) => {
+  // active rows have watched with no watched at end
+  // or last epi in last row watched
+  let watchedInLastEpi = false;
+  let inActiveRow      = false;
+
+  let waiting = false;  // unaired in active row (inc last epi)
+
+  let haveWatched            = false;
+  let nonWatchedAfterWatched = false;
+  let watchedGap             = false;  
+  let allWatched             = true;
+  let ready = false;
+
+  let nonFileAfterFile = false;
+  let fileGap          = false; 
+
   try {
     const seasonsRes = 
           await axios.get(urls.childrenUrl(cred, showId));
     for(let key in seasonsRes.data.Items) {
-      const seasonRec   =  seasonsRes.data.Items[key];
-      const seasonId    =  seasonRec.Id;
+      const season   = seasonsRes.data.Items[key];
+      const seasonId = season.Id;
+
+      const unairedObj = {};
+      const unairedRes = await axios.get(
+                urls.childrenUrl(cred, seasonId, true));
+      for(let key in unairedRes.data.Items) {
+        const episode       = unairedRes.data.Items[key];
+        const episodeNumber = +episode.IndexNumber;
+        unairedObj[episodeNumber] = true;
+      }
+
       const episodesRes = 
               await axios.get(urls.childrenUrl(cred, seasonId));
-      for(let key in episodesRes.data.Items) {
-        const episodeRec = episodesRes.data.Items[key];
-        const userData   = episodeRec?.UserData;
-        if(userData?.Played) watchedCount++;
+      const episodes =  episodesRes.data.Items;
+      for(let key in episodes) {
+        const episode       = episodes[key];
+        if(episode.IndexNumber === undefined) continue;
+        const showId        = episode.SeriesId;
+        const seasonId      = episode.SeasonId;
+        const episodeId     = episode.Id;
+        const episodeNumber = +episode.IndexNumber;
+        const userData      = episode?.UserData;
+        const watched       = !!userData?.Played;
+        const haveFile      = (episode.LocationType != "Virtual");
+        const unaired       = !!unairedObj[episodeNumber];
+        const imageTag      = episode?.ImageTags?.Primary;
+
+        let checkedReady = false;
+        let lastWatched  = false;
+
+        if(!checkedReady && lastWatched && !watched) {
+           checkedReady = true;
+           ready        = haveFile;
+        }
+
+        if(watched) haveWatched = true;
+        else        allWatched = false;
+
+        lastWatched = watched;
       }
     }
   }

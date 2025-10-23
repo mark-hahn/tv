b244525a4c1bfd6771cc80308af449616c06de39
Mark Hahn <mark@hahnca.com>
2025-10-23 15:17:28 -0700

    switched vite to wsl

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,16 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import envCompatible from 'vite-plugin-env-compatible';
-// import { injectHtml } from 'vite-plugin-html';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /^~/,
-        replacement: ''
-      },
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src')
-      }
-    ],
-    extensions: [
-      '.mjs',
-      '.js',
-      '.ts',
-      '.jsx',
-      '.tsx',
-      '.json',
-      '.vue'
-    ]
-  },
   plugins: [
     vue(),
-    vueJsx(),
-    viteCommonjs(),
-    envCompatible(),
-    // injectHtml()
   ],
-  server: {
-    strictPort: false,
-  },
-  build: {
-    outDir: "shows",
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })

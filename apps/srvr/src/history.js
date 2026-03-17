c142b1b2f87171a7e3e2c53d13dd4f69129e382c
Mark Hahn <mark@hahnca.com>
2026-03-17 16:44:01 -0700

    fixed history timestamps

--- apps/srvr/src/history.js
+++ apps/srvr/src/history.js
@@ -16,7 +16,7 @@ const PST_FMT = new Intl.DateTimeFormat("en-CA", {
   hour: "2-digit",
   minute: "2-digit",
   second: "2-digit",
-  hour12: false,
+  hourCycle: "h23",
 });
 
 const nowPST = () => {

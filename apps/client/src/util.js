32633ee1e3b540e919284f6e5dc4686a3bbe3d2d
Mark Hahn <mark@hahnca.com>
2026-03-18 18:23:14 -0700

    filter clnup

--- apps/client/src/util.js
+++ apps/client/src/util.js
@@ -156,22 +156,6 @@ export function setCondFltr(cond, fltrChoice) {
       tmp.full = 0;
       break;
 
-    case "Continue":
-      tmp.unplayed = 1;
-      tmp.gap = 0;
-      tmp.ended = 0;
-      tmp.waiting = 0;
-      tmp.drama = 0;
-      tmp.foreign = 0;
-      tmp.totry = 0;
-      tmp.continue = 1;
-      tmp.mark = 0;
-      tmp.linda = 0;
-      tmp.ban = -1;
-      tmp.hasemby = 1;
-      tmp.full = 0;
-      break;
-
     case "Finished":
       tmp.unplayed = 0;
       tmp.gap = 0;

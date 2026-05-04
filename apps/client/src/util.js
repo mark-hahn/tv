84760dc4f8d877c7333ce0512d6a0953a3ca67f7
Mark Hahn <mark@hahnca.com>
2026-05-03 17:29:53 -0700

    exclude waiting from needs files filter

--- apps/client/src/util.js
+++ apps/client/src/util.js
@@ -179,7 +179,7 @@ export function setCondFltr(cond, fltrChoice) {
       tmp.unplayed = -1;
       tmp.gap = 0;
       tmp.ended = 0;
-      tmp.waiting = 0;
+      tmp.waiting = -1;
       tmp.drama = 0;
       tmp.foreign = 0;
       tmp.totry = 0;
